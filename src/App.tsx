c2d7f9640e2fbecde0880eb9ad7213ce3e08193e
Vedran Blazenka <blazenka.vedran@gmail.com>
2020-06-21 11:33:24 +0200

    feat(chessboard): added chessboard and chessjs for validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import Chessboard from 'chessboardjsx';
+// @ts-ignore
+import Chess from 'chessjs'
+
 import './App.css';
 
 import './tailwind.generated.css'
 
 function App() {
+  const onGetPosition = React.useCallback((position) => {
+    console.log("@Position: ", position)
+  }, [])
+
   return (
     <div className="container xl mx-auto">
       <h1 className="font-sans text-lg text-purple-700 text-center">Hello Chess people!</h1>
+      <Chessboard getPosition={onGetPosition} position="start"/>
     </div>
   );
 }
