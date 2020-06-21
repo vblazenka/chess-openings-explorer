69e1d06d4c6dd097849baddc051591a258c1b2bc
Vedran Blazenka <blazenka.vedran@gmail.com>
2020-06-21 11:07:23 +0200

    feat(styles): add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import './tailwind.generated.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container xl mx-auto">
+      <h1 className="font-sans text-lg text-purple-700 text-center">Hello Chess people!</h1>
     </div>
   );
 }
